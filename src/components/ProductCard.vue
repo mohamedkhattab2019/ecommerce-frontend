600c7a806302eff86c0c03aa1e3b582adc4070ff
mohamedkhattab2019 <moero2019@gmail.com>
2024-11-27 22:33:54 +0200

    product avialbility

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,16 +1,23 @@
 <template>
-    <div class="bg-white rounded-md shadow-md pb-4 hover:shadow-lg w-5/6 mb-4">
+    <div class="bg-white rounded-md shadow-md pb-4 p-1 hover:shadow-lg w-5/6 mb-4">
       <img :src="product.image" alt="Product Image" class="w-full h-48 object-cover rounded-md mb-4" />
       <h3 class="text-lg font-bold text-gray-500 mb-2">{{ product.name_ar }}</h3>
-      <p class="text-sm text-gray-500">{{ product.description_ar }}</p>
-      <p class="text-lg font-semibold text-gray-800">{{ product.price }} EGP</p>
+      <p class="text-sm text-gray-500">{{ product.description_ar + product.quantity}}</p>
+      <p class="text-lg font-semibold text-gray-800">{{ product.price }} ر.س</p>
       <div class="flex item-center justify-center gap-2">
 
         <button
+          v-if="product.quantity > 0"
           class="bg-blue-500 text-white px-2 py-2 rounded-lg mt-4 hover:bg-blue-600"
           @click="$emit('add-to-cart', product.id)"
         >
           Add to Cart
+        </button>
+        <button
+          v-else
+          class="bg-red-500 text-white px-2 py-2 rounded-lg mt-4 hover:bg-blue-600"
+        >
+          غير متوفر
         </button>
         <button
           class="bg-blue-500 text-white px-2 py-2 rounded-lg mt-4 hover:bg-blue-600"
